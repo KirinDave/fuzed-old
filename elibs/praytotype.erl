01f2f5f96943be2917c7dcf5d1768b666ab94f1c
David Fayram <dfayram@gmail.com>
2007-06-25 15:42:02 -0700

    Removing some unnecessary files.

--- elibs/praytotype.erl
+++
@@ -1,9 +0,0 @@
--module(praytotype).
--compile(export_all).
-
-test() ->
-  Port = open_port({spawn, "ruby ./pray.rb"}, [{packet, 4}, use_stdio, exit_status, binary]),
-  Data = term_to_binary({hello, <<"hello world">>}),
-  Data2 = term_to_binary({whee, <<"Good times">>}),
-  Port ! {self(), {command, Data}},
-  Port ! {self(), {command, Data2}}.
